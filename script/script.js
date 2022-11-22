b477a864abf3162de2cfec533207ed6dee979d2f
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-22 10:23:13 -0800

    Added a removeEventListener

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
 const clickMeButton = document.querySelector(".click-me");
-function clickMe(){
-    alert("You Clicked!")
-}
-clickMeButton.addEventListener("click", clickMe);
+// function clickMe(){
+//     alert("You Clicked!")
+// }
 
+//Event listener only used once with that code at the bottom
+clickMeButton.addEventListener("click", function clickMe()
+{
+    alert("You Clicked!") 
+}, {once : true});
+
+//Event listener only used once with removeEventListener
+clickMeButton.addEventListener("click", function clickMe()
+{
+    alert("You Clicked!") 
+    clickMeButton.removeEventListener("click", clickMe);
+})
