ceea1ec44828a924129ebdc024deb929bcb56891
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-22 10:39:24 -0800

    added more event listeners

--- script/script.js
+++ script/script.js
@@ -4,14 +4,26 @@ const clickMeButton = document.querySelector(".click-me");
 // }
 
 //Event listener only used once with that code at the bottom
-clickMeButton.addEventListener("click", function clickMe()
-{
-    alert("You Clicked!") 
-}, {once : true});
+// clickMeButton.addEventListener("click", function clickMe()
+// {
+//     alert("You Clicked!") 
+// }, {once : true});
 
-//Event listener only used once with removeEventListener
+// //Event listener only used once with removeEventListener
+// clickMeButton.addEventListener("click", function clickMe()
+// {
+//     alert("You Clicked!") 
+//     clickMeButton.removeEventListener("click", clickMe);
+// })
+
 clickMeButton.addEventListener("click", function clickMe()
 {
-    alert("You Clicked!") 
-    clickMeButton.removeEventListener("click", clickMe);
+    // document.body.style.backgroundColor = 'pink';
+    document.body.classList.add("pink");
+})
+
+clickMeButton.addEventListener("click", function changeText(){
+    if (clickMeButton.textContent === "Click meh?"){
+        clickMeButton.textContent = "CLICK ME!?"}
+    document.body.clickMeButton.classList.add("change")
 })
